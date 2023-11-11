ff412b0ab74704f568fe979c9c46c723737358f6
faizttn <faijuddin@tothenew.com>
2023-11-11 22:40:48 +0530

    filter component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import Layout from "../../components/layout/Layout";
 import HeroSection from "../../components/heroSection/HeroSection";
+import Filter from "../../components/filter/Filter";
 
 function Home() {
   return (
     <Layout>
       <HeroSection />
+      <Filter />
     </Layout>
   );
 }
