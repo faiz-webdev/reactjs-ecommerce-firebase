b8fb151709a88e46138c958d3fd9737ef726adae
faizttn <faijuddin@tothenew.com>
2023-11-11 22:50:43 +0530

    product card component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import Layout from "../../components/layout/Layout";
 import HeroSection from "../../components/heroSection/HeroSection";
 import Filter from "../../components/filter/Filter";
+import ProductCard from '../../../../Complete-Ecommerce-Project-main/src/components/productCard/ProductCard';
 
 function Home() {
   return (
     <Layout>
       <HeroSection />
       <Filter />
+      <ProductCard />
     </Layout>
   );
 }
